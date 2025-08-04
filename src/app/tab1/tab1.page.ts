a222125b977ec92e60505922f147f89cc54d552c
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-08-04 15:02:45 +0200

    Add Home page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonGrid, IonRow, IonCol, IonSpinner, IonIcon
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {RouterLink} from "@angular/router";
+import {addIcons} from "ionicons";
+import {flameSharp, imagesOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, RouterLink, IonGrid, IonRow, IonCol, IonSpinner, IonIcon],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+    addIcons({ flameSharp, imagesOutline});
+  }
 }
