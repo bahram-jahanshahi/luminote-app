edf3ed8c60abda8eb1a2788b40e5c6b0a08f29f3
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-08-04 17:21:30 +0200

    Add the make the voice heard

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonSelect,
+  IonSelectOption,
+  IonText,
+  IonTextarea,
+  IonButton
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon, RouterLink, IonGrid, IonRow, IonCol, IonSelect, IonSelectOption, IonText, IonTextarea, IonButton]
 })
 export class Tab2Page {
 
